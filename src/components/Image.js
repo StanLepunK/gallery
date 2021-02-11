eef67a297cca4f3d98bc83f4f58fc7f4754a0614
Stan le Punk <stanislasiii@gmail.com>
2021-02-11 12:43:43 +0100

    update data.json

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 import { useContext } from 'react';
 import { Context } from '../KnupelContext';
 
+import useOver from '../hooks/useOver';
+
 import PropTypes from 'prop-types';
 
 Image.propTypes = {
@@ -16,7 +18,7 @@ Image.propTypes = {
 };
 
 function Image(props) {
-  const [is, set_is] = useState(false);
+  const [is, ref] = useOver();
   const { toggle_like, add_items, remove_items } = useContext(Context);
 
   function manage_items(elem) {
@@ -24,7 +26,6 @@ function Image(props) {
     if (!is) add_items(elem);
   }
 
-  // const cart_icon = is && <i className="ri-add-circle-line cart"></i>;
   function cart_icon(elem) {
     if (is)
       return (
@@ -44,8 +45,7 @@ function Image(props) {
   return (
     <div
       className={`${props.className} image-container`}
-      onMouseEnter={() => set_is(true)}
-      onMouseLeave={() => set_is(false)}
+      ref={ref}
       onClick={() => toggle_like(props.elem.id)}
     >
       {heart_icon(props.elem.isFavorite)}
@@ -56,3 +56,45 @@ function Image(props) {
 }
 
 export default Image;
+
+// function Image(props) {
+//   const [is, set_is] = useState(false);
+//   const { toggle_like, add_items, remove_items } = useContext(Context);
+
+//   function manage_items(elem) {
+//     const is = remove_items(elem.id);
+//     if (!is) add_items(elem);
+//   }
+
+//   // const cart_icon = is && <i className="ri-add-circle-line cart"></i>;
+//   function cart_icon(elem) {
+//     if (is)
+//       return (
+//         <i
+//           className="ri-add-circle-line cart"
+//           onClick={() => manage_items(elem)}
+//         ></i>
+//       );
+//   }
+
+//   function heart_icon(is_like) {
+//     if (is_like) return <i className="ri-heart-fill favorite"></i>;
+//     else if (is) return <i className="ri-heart-line favorite"></i>;
+//     else return <div></div>;
+//   }
+
+//   return (
+//     <div
+//       className={`${props.className} image-container`}
+//       onMouseEnter={() => set_is(true)}
+//       onMouseLeave={() => set_is(false)}
+//       onClick={() => toggle_like(props.elem.id)}
+//     >
+//       {heart_icon(props.elem.isFavorite)}
+//       {cart_icon(props.elem)}
+//       <img src={props.elem.url} className="image-grid"></img>
+//     </div>
+//   );
+// }
+
+// export default Image;
