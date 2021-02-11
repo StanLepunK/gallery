0d8e968f3e7b3b6581eb0b60be9f7ec1045b90f6
Stan le Punk <stanislasiii@gmail.com>
2021-02-11 12:45:38 +0100

    change ref of data file

--- src/KnupelContext.js
+++ src/KnupelContext.js
@@ -11,10 +11,10 @@ ContextProvider.propTypes = {
 function ContextProvider({ children }) {
   const [medias, set_medias] = useState([]);
   const [cart_items, set_cart_item] = useState([]);
-  // const address =
-  //   'https://raw.githubusercontent.com/StanLepunK/gallery/master/data/data.json';
   const address =
-    'https://raw.githubusercontent.com/StanLepunK/gallery/master/data/data_scrimba.json';
+    'https://raw.githubusercontent.com/StanLepunK/gallery/master/data/data.json';
+  // const address =
+  //   'https://raw.githubusercontent.com/StanLepunK/gallery/master/data/data_scrimba.json';
   useEffect(() => {
     fetch(address)
       .then((res) => res.json())
